c95db0fdf19a1326dcc78d480b5899b9ce2ce80e
jarvis5797 <ashutoshrajb@gmail.com>
2024-12-27 03:29:44 +0530

    added * for origin

--- src/main/java/com/creamybites/creamy_bites_server/config/WebConfig.java
+++ src/main/java/com/creamybites/creamy_bites_server/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") 
-                .allowedOrigins("http://localhost:5173" , "http://139.59.5.205:5173")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE" , "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
